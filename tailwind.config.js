cbfc5cc0c0d5df04670d1f7f4558f4fac7589a3f
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-03-12 04:37:33 +0800

    updated purge list

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,8 @@
 module.exports = {
     presets: [require("@felipage/tailwind-feli")],
-    purge: ["./node_modules/\\@felipage/react-ui/**/*.{js,ts,jsx,tsx}"],
+    purge: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./node_modules/\\@felipage/react-ui/**/*.{js,ts,jsx,tsx}",
+    ],
 };
