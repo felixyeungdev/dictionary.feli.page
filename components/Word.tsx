d9746a1a999b38fbefc86202aa08fdeab48c0b88
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-03-12 12:10:39 +0800

    changed opposite to red colour

--- components/Word.tsx
+++ components/Word.tsx
@@ -88,7 +88,7 @@ const Word = ({ data }: Props) => {
                                             )}
                                             {antonyms && (
                                                 <div className="flex items-center gap-3 mt-2">
-                                                    <span className="text-green-600">
+                                                    <span className="text-red-600">
                                                         Opposite:
                                                     </span>
                                                     <div className="flex flex-wrap gap-3">
