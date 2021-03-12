653bf0cb7414fd39f65ce28ef6cb7506a61ed5ee
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-03-12 15:13:04 +0800

    loading animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
-import { Content, SlowTextField } from "@felipage/react-ui";
-import React, { useEffect } from "react";
+import { Alert, Content, SlowTextField } from "@felipage/react-ui";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Word from "../components/Word";
 import { IWord } from "../interfaces/word";
 import PageHead from "../components/PageHead";
+import { motion, AnimatePresence } from "framer-motion";
 
 interface Props {
     word?: string;
@@ -12,6 +13,7 @@ interface Props {
 
 const IndexPage = ({ word, data }: Props) => {
     const router = useRouter();
+    const [loading, setLoading] = useState(false);
 
     const onChange = async (value: string) => {
         if (!value) {
@@ -34,22 +36,58 @@ const IndexPage = ({ word, data }: Props) => {
         onChange(word ?? "");
     }, [word]);
 
+    useEffect(() => {
+        let routeChangeStart = () => {
+            setLoading(true);
+        };
+        let routeChangeComplete = () => {
+            setLoading(false);
+        };
+
+        router.events.on("routeChangeStart", routeChangeStart);
+        router.events.on("routeChangeComplete", routeChangeComplete);
+        router.events.on("routeChangeError", routeChangeComplete);
+        return () => {
+            router.events.off("routeChangeStart", routeChangeStart);
+            router.events.off("routeChangeComplete", routeChangeComplete);
+            router.events.off("routeChangeError", routeChangeComplete);
+        };
+    }, []);
+
     return (
-        <Content>
-            <PageHead word={word} data={data} />
-            <div className="mt-3">
-                <SlowTextField
-                    fullWidth
-                    label="Word"
-                    onChange={onChange}
-                    value={word ?? ""}
-                    placeholder="Start searching"
-                />
-            </div>
-            <div className="mt-3">
-                <Word data={data} />
-            </div>
-        </Content>
+        <>
+            <Content>
+                <PageHead word={word} data={data} />
+                <div className="mt-3">
+                    <SlowTextField
+                        fullWidth
+                        label="Word"
+                        onChange={onChange}
+                        value={word ?? ""}
+                        placeholder="Start searching"
+                    />
+                </div>
+                <div className="mt-3 mb-12">
+                    <Word data={data} />
+                </div>
+            </Content>
+            <AnimatePresence>
+                {loading && (
+                    <motion.div
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        transition={{
+                            duration: 0.1,
+                            damping: 10,
+                            stiffness: 100,
+                        }}
+                    >
+                        <Alert fixed>Loading</Alert>
+                    </motion.div>
+                )}
+            </AnimatePresence>
+        </>
     );
 };
 
