7fb5a40c1e6c91c87d02ea584e17d6f6517db4f8
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-03-14 00:56:10 +0800

    added settings page

---
+++ pages/settings.tsx
@@ -0,0 +1,24 @@
+import { Button, Content } from "@felipage/react-ui";
+import { useRouter } from "next/router";
+import React from "react";
+import { useLocalStorage } from "react-use";
+
+const Settings = () => {
+    const [history, _setHistory, clearHistory] = useLocalStorage<string[]>(
+        "search-history",
+        []
+    );
+
+    return (
+        <Content>
+            <div className="mt-3">
+                <h1>Settings</h1>
+                <h2>Search History</h2>
+                <p>{history?.length ?? 0} entries</p>
+                <Button onClick={clearHistory}>Clear Search History</Button>
+            </div>
+        </Content>
+    );
+};
+
+export default Settings;
